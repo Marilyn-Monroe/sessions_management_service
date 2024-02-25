ff87f60ff5971b645883f992f0f5dfb8ddf08676
Marilyn-Monroe <norma.jeane.mortenson@outlook.com>
2024-02-25 16:11:49 +0300

    Not pass grpc-server.port in the static config

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
-#include <userver/components/minimal_server_component_list.hpp>
 #include <userver/clients/dns/component.hpp>
 #include <userver/clients/http/component.hpp>
+#include <userver/components/minimal_server_component_list.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
+#include <userver/ugrpc/server/server_component.hpp>
 #include <userver/utils/daemon_run.hpp>
 
 #include "hello.hpp"
@@ -14,7 +15,8 @@ int main(int argc, char* argv[]) {
                             .Append<userver::components::TestsuiteSupport>()
                             .Append<userver::components::HttpClient>()
                             .Append<userver::clients::dns::Component>()
-                            .Append<userver::server::handlers::TestsControl>();
+                            .Append<userver::server::handlers::TestsControl>()
+                            .Append<userver::ugrpc::server::ServerComponent>();
 
   sessions_management_service::AppendHello(component_list);
 
---
+++ src/signin.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 2/24/24.
+//
+
+#include "signin.h"
